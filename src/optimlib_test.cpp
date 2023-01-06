7ef738eb1b6b2abdae8abeccd0869602629504a4
waxz <wz677@163.com>
2023-01-07 02:59:39 +0800

    update optimlib test demo;

--- src/optimlib_test.cpp
+++ src/optimlib_test.cpp
@@ -38,46 +38,46 @@ opt_fn(const Eigen::VectorXd &x, Eigen::VectorXd *grad_out, void *opt_data) {
     return autodiff::val(y);
 }
 
+struct Point{
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
+    float r = 0.0;
+};
+
 
 autodiff::var
-circle_opt_fnd(const autodiff::ArrayXvar& x, const std::vector<float>& points, float radius_2)
+circle_opt_fnd(const autodiff::ArrayXvar& x, const std::vector<Point>& points,int point_num, float radius_2)
 {
     autodiff::var r = 0.0;
     autodiff::var t = 0.0;
 
-    for(int i = 0 ; i < 50;i++){
-        t = (x(0) - points[i+i])*(x(0) - points[i+i]) + (x(1) - points[i+i+1])*(x(1) - points[i+i+1]) - radius_2;
+    for(int i = 0 ; i < point_num ;i++){
+        t = (x(0) - points[i].x)*(x(0) - points[i].x) + (x(1) - points[i].y)*(x(1) - points[i].y) - radius_2;
         r += t*t;
-//        r += t;
-
     }
     return r;
 }
-struct CostFunction{
 
-    std::vector<float> points;
+struct CircleCostFunction{
+
+    const std::vector<Point>& points;
+    int point_num;
     float radius = 0.1;
+    float radius_2 = radius;
 
 
-    CostFunction(float cx, float cy, float r):radius(r){
-
-        points.resize(100);
-        float inc = M_PI / 50.0;
-//        std::cout << "\nlist points:\n";
-
-        for(int i = 0; i < 50;i++){
-            points[i+i] = cx + std::cos(inc*i)*radius;
-            points[i+i+1] = cy + std::sin(inc*i)*radius;
-//            std::cout <<points[i+i] << ", " <<points[i+i+1] << "\n";
-        }
+    CircleCostFunction(const std::vector<Point>& t_points, int t_point_num, float t_radius):points(t_points),point_num(t_point_num), radius(t_radius),radius_2(radius*radius){
     }
+
+
 
     double operator()(const Eigen::VectorXd& x, Eigen::VectorXd* grad_out, void* opt_data)
     {
 
         autodiff::ArrayXvar xd = x.eval();
 
-        autodiff::var y = circle_opt_fnd(xd,points,radius*radius);
+        autodiff::var y = circle_opt_fnd(xd,points,point_num,radius_2);
 
         if (grad_out) {
             Eigen::VectorXd grad_tmp = autodiff::gradient(y, xd);
@@ -88,7 +88,6 @@ struct CostFunction{
         return autodiff::val(y);
     }
 };
-
 
 void test_1() {
     Eigen::VectorXd x(5);
@@ -107,10 +106,23 @@ void test_1() {
 }
 
 void test_2(){
-    Eigen::VectorXd x(2);
-    x << 0.04, 0.06;
 
-    CostFunction opt_fn_obj(0.12,0.08,0.1) ;
+    int point_num = 100;
+    std::vector<Point> points(point_num);
+    float angle_inc = 2.0f*M_PI/float(point_num);
+
+    float sample_cx = 0.13, sample_cy = 0.06, sample_radius = 0.1;
+
+    for(int i = 0 ; i < point_num;i++){
+        points[i].x = sample_cx + sample_radius*std::cos(i*angle_inc);
+        points[i].y = sample_cy + sample_radius*std::sin(i*angle_inc);
+
+    }
+
+    Eigen::VectorXd x(2);
+    x << 0.05, 0.05;
+
+    CircleCostFunction opt_fn_obj(points,point_num, sample_radius) ;
 
     bool success = optim::bfgs(x, opt_fn_obj, nullptr);
 
