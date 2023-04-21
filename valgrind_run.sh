1c2a077785d6dfbcc81ebc3ae543eaf5642fae66
waxz <wz67766@163.com>
2023-04-22 00:28:03 +0800

    add valgrind test script

--- valgrind_run.sh
+++ valgrind_run.sh
@@ -3,5 +3,6 @@
 #"sudo apt-get install valgrind"
 
 path=$(date +'%H-%M-%S')
-echo "write log to valgrind-$path.log"
 G_SLICE=always-malloc G_DEBUG=gc-friendly  valgrind -v --tool=memcheck --leak-check=full --num-callers=40 --log-file=valgrind-$path.log $@
+echo "write log to valgrind-$path.log"
+
